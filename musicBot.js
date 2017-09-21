d4c14881abae43097e529529e0016fd6a47bc61f
Freeman12112 <32149762+Freeman12112@users.noreply.github.com>
2017-09-21 13:02:34 -0400

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -1,10 +1,15 @@
 const Discord = require('discord.js');
 const music = require('discord.js-music-v11');
 const client = new Discord.Client();
+const Bot = new Discord.Client();
 const token = "<auth_token>" // Recommended to load from json file.
 
 client.on('ready', () => {
     console.log('I am ready!');
+});
+
+Bot.on('ready', () => {
+    console.log(`[Start] ${new Date()}`);
 });
 
 music(Bot, {
@@ -16,4 +21,5 @@ music(Bot, {
 });
 
 // THIS  MUST  BE  THIS  WAY
+Bot.login(token);
 client.login(process.env.BOT_TOKEN);
